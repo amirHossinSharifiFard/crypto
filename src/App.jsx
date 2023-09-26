0d6068375a292ac9e80cca9e45be6645ab6f46b7
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2023-09-26 08:13:52 +0330

    # do

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+// import { Route, Routes, Navigate } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <div className='App'>
+
+    </div>
+  );
+};
+
+export default App;
